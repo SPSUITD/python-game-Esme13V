277172a7d86944db5037c83676eca44dee08f70c
Esme13V <106529649+Esme13V@users.noreply.github.com>
2022-05-31 20:18:41 +0300

    Add files via upload

---
+++ running from dogs.py
@@ -0,0 +1,223 @@
+import pygame, random, sys
+from pygame.locals import *
+
+WIDTH = 600
+HEIGHT = 600
+TEXTCOLOR = (255, 255, 255)
+TEXTCOLOR1 = (0, 0, 0)
+BACKGROUNDCOLOR = (153, 50, 204)
+FPS = 40
+BADDIEMINSIZE = 10
+BADDIEMAXSIZE = 40
+BADDIEMINSPEED = 1
+BADDIEMAXSPEED = 8
+ADDNEWBADDIERATE = 6
+PLAYERMOVERATE = 5
+
+gameDisplay = pygame.display.set_mode((WIDTH,HEIGHT))
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def waitForPlayerToPressKey():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE: # pressing escape quits
+                    terminate()
+                return
+
+def playerHasHitBaddie(playerRect, baddies):
+    for b in baddies:
+        if playerRect.colliderect(b['rect']):
+            return True
+    return False
+
+def drawText(text, font, surface, x, y):
+    textobj = font.render(text, 1, TEXTCOLOR)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+def drawText1(text, font, surface, x, y):
+    textobj = font.render(text, 1, TEXTCOLOR1)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+# set up pygame, the window, and the mouse cursor
+pygame.init()
+mainClock = pygame.time.Clock()
+windowSurface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('running from dogs')
+pygame.mouse.set_visible(False)
+
+# set up fonts
+font = pygame.font.SysFont(None, 48)
+
+# set up sounds
+gameOverSound = pygame.mixer.Sound('gameover.mp3')
+pygame.mixer.music.load('background.mp3')
+
+# set up images
+playerImage = pygame.image.load('cat.png')
+playerRect = playerImage.get_rect()
+baddieImage = pygame.image.load('dog.png')
+
+background = pygame.image.load('background.png').convert()
+background = pygame.transform.smoothscale(background, gameDisplay.get_size())
+background1 = pygame.image.load('background1.png').convert()
+background1 = pygame.transform.smoothscale(background1, gameDisplay.get_size())
+background2 = pygame.image.load('background2.png').convert()
+background2 = pygame.transform.smoothscale(background2, gameDisplay.get_size())
+
+# draw the background
+gameDisplay.blit(background2, (0, 0))
+
+# show the "Start" screen
+drawText1('Бег от собак', font, windowSurface, (WIDTH / 3), (HEIGHT / 3))
+drawText1('Спасите котика от злых собак!', font, windowSurface, (WIDTH / 7) - 30, (HEIGHT / 3) + 50)
+pygame.display.update()
+waitForPlayerToPressKey()
+
+
+topScore = 0
+while True:
+    # set up the start of the game
+    baddies = []
+    score = 0
+    playerRect.topleft = (WIDTH / 2, HEIGHT - 50)
+    moveLeft = moveRight = moveUp = moveDown = False
+    reverseCheat = slowCheat = False
+    baddieAddCounter = 0
+    pygame.mixer.music.play(-1, 0.0)
+
+    while True: # the game loop runs while the game part is playing
+        score += 1 # increase score
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+
+            if event.type == KEYDOWN:
+                if event.key == ord('z'):
+                    reverseCheat = True
+                if event.key == ord('x'):
+                    slowCheat = True
+                if event.key == K_LEFT or event.key == ord('a'):
+                    moveRight = False
+                    moveLeft = True
+                if event.key == K_RIGHT or event.key == ord('d'):
+                    moveLeft = False
+                    moveRight = True
+                if event.key == K_UP or event.key == ord('w'):
+                    moveDown = False
+                    moveUp = True
+                if event.key == K_DOWN or event.key == ord('s'):
+                    moveUp = False
+                    moveDown = True
+
+            if event.type == KEYUP:
+                if event.key == ord('z'):
+                    reverseCheat = False
+                    score = 0
+                if event.key == ord('x'):
+                    slowCheat = False
+                    score = 0
+                if event.key == K_ESCAPE:
+                        terminate()
+
+                if event.key == K_LEFT or event.key == ord('a'):
+                    moveLeft = False
+                if event.key == K_RIGHT or event.key == ord('d'):
+                    moveRight = False
+                if event.key == K_UP or event.key == ord('w'):
+                    moveUp = False
+                if event.key == K_DOWN or event.key == ord('s'):
+                    moveDown = False
+
+            if event.type == MOUSEMOTION:
+                # If the mouse moves, move the player where the cursor is.
+                playerRect.move_ip(event.pos[0] - playerRect.centerx, event.pos[1] - playerRect.centery)
+
+        # Add new baddies at the top of the screen, if needed.
+        if not reverseCheat and not slowCheat:
+            baddieAddCounter += 1
+        if baddieAddCounter == ADDNEWBADDIERATE:
+            baddieAddCounter = 0
+            baddieSize = random.randint(BADDIEMINSIZE, BADDIEMAXSIZE)
+            newBaddie = {'rect': pygame.Rect(random.randint(0, HEIGHT-baddieSize), 0 - baddieSize, baddieSize, baddieSize),
+                        'speed': random.randint(BADDIEMINSPEED, BADDIEMAXSPEED),
+                        'surface':pygame.transform.scale(baddieImage, (baddieSize, baddieSize)),
+                        }
+
+            baddies.append(newBaddie)
+
+        # Move the player around.
+        if moveLeft and playerRect.left > 0:
+            playerRect.move_ip(-1 * PLAYERMOVERATE, 0)
+        if moveRight and playerRect.right < WIDTH:
+            playerRect.move_ip(PLAYERMOVERATE, 0)
+        if moveUp and playerRect.top > 0:
+            playerRect.move_ip(0, -1 * PLAYERMOVERATE)
+        if moveDown and playerRect.bottom < HEIGHT:
+            playerRect.move_ip(0, PLAYERMOVERATE)
+
+        # Move the mouse cursor to match the player.
+        pygame.mouse.set_pos(playerRect.centerx, playerRect.centery)
+
+        # Move the baddies down.
+        for b in baddies:
+            if not reverseCheat and not slowCheat:
+                b['rect'].move_ip(0, b['speed'])
+            elif reverseCheat:
+                b['rect'].move_ip(0, -5)
+            elif slowCheat:
+                b['rect'].move_ip(0, 1)
+
+         # Delete baddies that have fallen past the bottom.
+        for b in baddies[:]:
+            if b['rect'].top > HEIGHT:
+                baddies.remove(b)
+
+        # draw the background
+        gameDisplay.blit(background1, (0, 0))
+
+        # Draw the score and top score.
+        drawText1('Рекорд: %s' % (score), font, windowSurface, 10, 0)
+        drawText1('Лучший рекорд: %s' % (topScore), font, windowSurface, 10, 40)
+
+        # Draw the player's rectangle
+        windowSurface.blit(playerImage, playerRect)
+
+        # Draw each baddie
+        for b in baddies:
+            windowSurface.blit(b['surface'], b['rect'])
+
+        pygame.display.update()
+
+        # Check if any of the baddies have hit the player.
+        if playerHasHitBaddie(playerRect, baddies):
+            if score > topScore:
+                topScore = score # set new top score
+            break
+
+        mainClock.tick(FPS)
+
+    # draw the background
+    gameDisplay.blit(background, (0, 0))
+
+    # Stop the game and show the "Game Over" screen.
+    pygame.mixer.music.stop()
+    gameOverSound.play()
+
+    drawText('GAME OVER', font, windowSurface, (WIDTH / 3), (HEIGHT / 6))
+    drawText('Вашего котика съела собака.', font, windowSurface, (WIDTH / 4) - 80, (HEIGHT / 4))
+    drawText('Попробуйте еще раз!', font, windowSurface, (WIDTH / 3) - 80, (HEIGHT / 3))
+    pygame.display.update()
+    waitForPlayerToPressKey()
+
+    gameOverSound.stop()
